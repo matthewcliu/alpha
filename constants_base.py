58d609eb4037b7aeca21107d704e5b906f286861
Matthew Liu <matthewliu@gmail.com>
2012-11-30 00:07:46 -0800

    Project scaffold

---
+++ constants_base.py
@@ -0,0 +1,10 @@
+BASE_PATH = '/var/www/vmserver/alpha/'
+
+APP = 'alpha'
+
+DB_ENGINE = 'django.db.backends.mysql'
+DB_NAME = 'alpha'
+DB_USER = 'root'
+DB_PASSWORD = 'password'
+DB_HOST = ''
+DB_PORT = '3306'
---
+++ myapp1/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+
+# Create your models here.
+
+class Chick(models.Model):
+    first_name = models.CharField(max_length=200)
+    last_name = models.CharField(max_length=200)
+    
